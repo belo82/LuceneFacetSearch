9484bb5741999becf6c57c3d2e972b4289854081
Peter Belko <peter.belko@gmail.com>
2013-10-13 15:14:28 +0100

    intial project structure

---
+++ src/test/java/com/belo82/facetsearch/MainTest.java
@@ -0,0 +1,18 @@
+package com.belo82.facetsearch;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple Main.
+ */
+public class MainTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        Assert.assertTrue(true);
+    }
+}
